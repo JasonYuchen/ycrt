25169634ff157ee4648728f4e1a3abc415a55ecb
jasonyuchen <jasonyuchen@foxmail.com>
2020-01-12 16:31:24 +0800

    add TestHack for class testing

---
+++ tests/TestHack.h
@@ -0,0 +1,13 @@
+//
+// Created by jason on 2020/1/12.
+//
+
+#ifndef YCRT_TESTS_TESTHACK_H_
+#define YCRT_TESTS_TESTHACK_H_
+
+#ifdef GTEST
+#define private public
+#define protected public
+#endif
+
+#endif //YCRT_TESTS_TESTHACK_H_
