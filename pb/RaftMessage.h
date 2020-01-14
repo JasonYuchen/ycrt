89c86a7685910ad0a3c484dd273d4e87767a9604
jasonyuchen <jasonyuchen@foxmail.com>
2020-01-14 13:42:13 +0800

    add types

--- pb/RaftMessage.h
+++ pb/RaftMessage.h
@@ -6,6 +6,7 @@
 #define YCRT_PB_RAFTMESSAGE_H_
 
 #include "raft.pb.h"
+#include <vector>
 
 namespace ycrt
 {
@@ -59,6 +60,53 @@ typedef struct ReadyToRead {
 } pbReadyToRead;
 
 constexpr uint8_t NumOfMessageType = 28;
+
+typedef struct UpdateCommit {
+  uint64_t Processed;
+  uint64_t LastApplied;
+  uint64_t StableLogTo;
+  uint64_t StableLogTerm;
+  uint64_t StableSnapshotTo;
+  uint64_t ReadyToRead;
+} pbUpdateCommit;
+
+typedef struct Update {
+  uint64_t ClusterID;
+  uint64_t NodeID;
+  // The current persistent state of a raft node. It must be stored onto
+  // persistent storage before any non-replication can be sent to other nodes.
+  // isStateEqual(emptyState) returns true when the state is empty.
+  pbState State;
+  // whether CommittedEntries can be applied without waiting for the Update
+  // to be persisted to disk
+  bool FastApply;
+  // EntriesToSave are entries waiting to be stored onto persistent storage.
+  std::vector<pbEntry> EntriesToSave;
+  // CommittedEntries are entries already committed in raft and ready to be
+  // applied by dragonboat applications.
+  std::vector<pbEntry> CommittedEntries;
+  // Whether there are more committed entries ready to be applied.
+  bool MoreCommittedEntries;
+  // Snapshot is the metadata of the snapshot ready to be applied.
+  pbSnapshotUPtr Snapshot;
+  // ReadyToReads provides a list of ReadIndex requests ready for local read.
+  std::vector<pbReadyToRead> ReadyToReads;
+  // Messages is a list of outgoing messages to be sent to remote nodes.
+  // As stated above, replication messages can be immediately sent, all other
+  // messages must be sent after the persistent state and entries are saved
+  // onto persistent storage.
+  std::vector<pbMessage> Messages;
+  // LastApplied is the actual last applied index reported by the RSM.
+  uint64_t LastApplied;
+  // UpdateCommit contains info on how the Update instance can be committed
+  // to actually progress the state of raft.
+  pbUpdateCommit UpdateCommit;
+  // DroppedEntries is a list of entries dropped when no leader is available
+  std::vector<pbEntry> DroppedEntries;
+  // DroppedReadIndexes is a list of read index requests  dropped when no leader
+  // is available.
+  std::vector<pbSystemCtx> DroppedReadIndexes;
+} pbUpdate;
 
 } // namespace ycrt
 
